25380e964c2e347a11bd22c0997ce5d17c10348d
SUMIT <sumitdogra3236@gmail.com>
2021-09-03 22:59:10 +0530

    Java

--- Java Zero to Hero/Day_1.java
+++ Java Zero to Hero/Day_1.java
@@ -1,3 +1,5 @@
+import javax.print.event.PrintJobListener;
+
 /*
 James Arthur Gosling -> Father of Java - 1995
 Sun-MicroSystem -> Oracle
@@ -25,8 +27,43 @@ Execute the code
 */
 
 
-class Demo {
+// class Demo {
+//     public static void main(String[] args) {
+//         System.out.println("Hello there!");
+//     }
+// }
+
+/*
+OOP Vs POP
+
+OOP - bottom up approach, focuses on data
+POP - Top Down Approach, focuses more on performing the task 
+
+
+Classes & Object:
+Class - is a a collection of similar object, its a blueprint (set of features, specifications) &
+Object - is a instance of a class, object implement those features
+
+Memory is not allocated for the class, when we declare the object, then the memory is allocated for that object.
+Class doesn't take space, object take space
+*/
+
+
+class Test {
+    
+        int x; // instance variable or object variable
+        void show() { // method -> function defined inside a class
+            System.out.println("Hello from Class Test");
+        }
+    
+}
+
+class TestDrive {
     public static void main(String[] args) {
-        System.out.println("Hello there!");
+        Test ob = new Test();
+        ob.x = 20;
+        System.out.println("Data: " +ob.x );
+        ob.show();
     }
+
 }
