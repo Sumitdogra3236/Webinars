b97128bcf9c424cfee58f7cf8e5e303e85a8e593
SUMIT <sumitdogra3236@gmail.com>
2021-09-03 22:29:32 +0530

    Java

---
+++ Java Zero to Hero/Day_1.java
@@ -0,0 +1,32 @@
+/*
+James Arthur Gosling -> Father of Java - 1995
+Sun-MicroSystem -> Oracle
+
+*/
+
+/*
+JVM - Java Virtual Machine 
+JRE - Java Runtime Environment - provides the runtime environment for the code
+JDK - Java Development Kit - its a kit, contains other libraries
+
+C/C++ Programs -> directly interact with OS
+Java Programs-> intracts with JVM
+
+Java is platform dependent - it is due to JVM
+JVM is patform dependent
+
+Tasks of JVM:
+Load the code
+Verify the code 
+Provide JRE
+Execute the code
+
+
+*/
+
+
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+    }
+}
