9f42b0a417c36ad64bf55800a3baf02112853891
SUMIT <sumitdogra3236@gmail.com>
2021-09-04 00:54:25 +0530

    HW 2

---
+++ HW2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int sum = x+y;
+        System.out.println("Sum is "+ sum);
+    }
+}
