c3368bdd80c511aa42d8734f47caac8c760776eb
SUMIT <sumitdogra3236@gmail.com>
2021-09-03 22:13:20 +0530

    Java

---
+++ Java Programming/Day_1.java
@@ -0,0 +1,25 @@
+/*
+James Arthur Gosling -> Father of Java - 1995
+Sun-MicroSystem -> Oracle
+
+*/
+
+/*
+JVM - Java Virtual Machine 
+JRE - Java Runtime Environment
+JDK - Java Development Kit
+
+C/C++ Programs -> directly interact with OS
+Java Programs-> intracts with JVM
+
+Java is platform dependent - it is due to JVM
+JVM is patform dependent
+
+Tasks of JVM:
+Load the code
+Verify the code 
+Provide JRE
+Execute the code
+
+
+*/
