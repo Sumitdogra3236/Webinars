13ccdd2aa0eebe4597cd88a0e961b1b4345d4563
SUMIT <sumitdogra3236@gmail.com>
2021-09-08 08:58:33 +0530

    HW

---
+++ JavaScript Zero to Hero/HomeWork/HW1/HW1.js
@@ -0,0 +1,17 @@
+var num1 = 8;
+var num2 = 2;
+
+var add = num1+num2;
+console.log(`Sum of ${num1} and ${num2} is: ${add}`);
+
+var sub = num1-num2;
+console.log(`Subtraction of ${num1} and ${num2} is: ${sub}`);
+
+var mul = num1 * num2;
+console.log(`Multiplication of ${num1} and ${num2} is: ${mul}`);
+
+var div = num1/num2;
+console.log(`Division of ${num1} and ${num2} is: ${div}`);
+
+var mod = num1%num2;
+console.log(`Mod of ${num1} and ${num2} is: ${mod}`);
