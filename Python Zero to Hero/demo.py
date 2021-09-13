a066b4bf01b232e2e555fe0c3301df66b0dcc0e6
SUMIT <sumitdogra3236@gmail.com>
2021-09-14 00:06:54 +0530

    Python

--- Python Zero to Hero/demo.py
+++ Python Zero to Hero/demo.py
@@ -19,17 +19,35 @@
 a = 400;
 b = a;
 c = 400;
-print(id(a));   #-------> 2884405803312
-print(id(b));   #-------> 2884405803312
-print(id(c));   #-------> 2884405803088
+# print(id(a));   #-------> 2884405803312
+# print(id(b));   #-------> 2884405803312
+# print(id(c));   #-------> 2884405803088
 
 # Operator 
 # and Operator -> if and operator gets the first condition false , it will never check the second condition, and return false
 
-print(10 > 5 and 3 < 4); # ----> True
-print(10 > 5 and 3 < Sumit) # ----> Error -> name 'Sumit' is not defined 
-print(10 < 5 and 3 < Sumit) # ----> False 
+# print(10 > 5 and 3 < 4); # ----> True
+# print(10 > 5 and 3 < Sumit) # ----> Error -> name 'Sumit' is not defined 
+# print(10 < 5 and 3 < Sumit) # ----> False 
 
 # or Operator -> if one of the expresion is True gives true 
-print(10 > 20 or 5 > Sumit) # ----> Error -> name 'Sumit' is not defined
-print(20 > 10 or 5 > Sumit) # ----> True
+# print(10 > 20 or 5 > Sumit) # ----> Error -> name 'Sumit' is not defined
+# print(20 > 10 or 5 > Sumit) # ----> True
+
+# in operator -> membership operator
+a = "PythonProgramming";
+print("P" in a); # ----> True
+print("X" in a); # ----> False
+print("X" not in a); # Inverter ----> True
+
+# is operator -> identity operator
+# return that the variable pointed to same memory object or not
+x = 30
+y = 30
+print(x == y) # ----> True
+print(x is y) # ----> True
+
+m = 300;
+n = 300;
+print(m == n) # ----> True
+print(m is n) # ----> False
